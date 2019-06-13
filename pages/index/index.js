12b71e677043a92fe1ecdd67545a367cee02b6c0
lrqnew <1406107400@qq.com>
2019-06-13 14:50:14 +0800

    完成文字识别

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,39 @@
 Page({
     data: {
-        array: [ "中英文混合", "英文", "葡萄牙语", "法语", "德语", "意大利语", "西班牙语", "俄语", "日语", "韩语" ],
-        objectArray: [ {
-            id: "CHN_ENG"
-        }, {
-            id: 1,
-            name: "ENG"
-        }, {
-            id: 2,
-            name: "POR"
-        }, {
-            id: 3,
-            name: "FRE"
-        }, {
-            id: 4,
-            name: "GER"
-        }, {
-            id: 5,
-            name: "ITA"
-        }, {
-            id: 6,
-            name: "SPA"
-        }, {
-            id: 7,
-            name: "RUS"
-        }, {
-            id: 8,
-            name: "JAP"
-        }, {
-            id: 9,
-            name: "KOR"
-        } ],
-        index: 0,
-        lang: "",
-        imgalist: [ "https://wx.wicode.cn/images/code.jpg" ],
+        // array: [ "中英文混合", "英文", "葡萄牙语", "法语", "德语", "意大利语", "西班牙语", "俄语", "日语", "韩语" ],
+        // objectArray: [ {
+        //     id: "CHN_ENG"
+        // }, {
+        //     id: 1,
+        //     name: "ENG"
+        // }, {
+        //     id: 2,
+        //     name: "POR"
+        // }, {
+        //     id: 3,
+        //     name: "FRE"
+        // }, {
+        //     id: 4,
+        //     name: "GER"
+        // }, {
+        //     id: 5,
+        //     name: "ITA"
+        // }, {
+        //     id: 6,
+        //     name: "SPA"
+        // }, {
+        //     id: 7,
+        //     name: "RUS"
+        // }, {
+        //     id: 8,
+        //     name: "JAP"
+        // }, {
+        //     id: 9,
+        //     name: "KOR"
+        // } ],
+        // index: 0,
+        // lang: "",
+        // imgalist: [ "https://wx.wicode.cn/images/code.jpg" ],
     },
 
     previewImage: function(a) {
@@ -82,9 +82,33 @@ Page({
         url: '../wpsbIndex/wpsbIndex',
       })
     },
+
     Face: function(a) {
-      wx.navigateTo({
-        url: "../wpsbIndex/wpsbIndex"
+      wx.chooseImage({
+        count: 1,
+        sizeType: ["original", "compressed"],
+        sourceType: ["album", "camera"],
+        success: function (a) {
+          var e = a.tempFilePaths;
+          getApp().globalData.img = e, wx.showLoading({
+            title: "正在识别"
+          }), wx.uploadFile({
+            url: "https://wx.oneint.cn/face.ashx",
+            filePath: e[0],
+            name: "file",
+            formData: {
+              user: "test"
+            },
+            success: function (a) {
+              "0" == a.data ? (wx.showModal({
+                title: "错误！",
+                content: "这不是人脸"
+              }), wx.hideLoading()) : (wx.hideLoading(), wx.navigateTo({
+                url: "../face/face?list=" + a.data
+              }));
+            }
+          });
+        }
       });
     },
     PhoneInfo: function(a) {
@@ -137,32 +161,10 @@ Page({
             url: "../about/about"
         });
     },
+    //跳转文字识别
     bindPickerChange: function(a) {
-        var e = this;
-        e.setData({
-            lang: e.data.objectArray[a.detail.value].id
-        }), wx.chooseImage({
-            count: 1,
-            sizeType: [ "original", "compressed" ],
-            sourceType: [ "album", "camera" ],
-            success: function(a) {
-                var t = a.tempFilePaths;
-                getApp().globalData.img = t, wx.showLoading({
-                    title: "正在识别"
-                }), wx.uploadFile({
-                  url: "https://wx.wicode.cn/Wenzi.ashx?lang=" + e.data.lang,
-                    filePath: t[0],
-                    name: "file",
-                    formData: {
-                        user: "test"
-                    },
-                    success: function(a) {
-                        wx.hideLoading(), wx.navigateTo({
-                            url: "../font/font?list=" + a.data
-                        }), console.log(a.data);
-                    }
-                });
-            }
-        });
+        wx.navigateTo({
+          url: '../wzsbIndex/wzsbIndex',
+        })
     }
 });
---
+++ pages/wzsbIndex/wzsbIndex.js
@@ -0,0 +1,127 @@
+// pages/wzsbIndex/wzsbIndex.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ["中英文混合", "英文", "葡萄牙语", "法语", "德语", "意大利语", "西班牙语", "俄语", "日语", "韩语"],
+    objectArray: [{
+      id: "CHN_ENG"
+    }, {
+      id: 1,
+      name: "ENG"
+    }, {
+      id: 2,
+      name: "POR"
+    }, {
+      id: 3,
+      name: "FRE"
+    }, {
+      id: 4,
+      name: "GER"
+    }, {
+      id: 5,
+      name: "ITA"
+    }, {
+      id: 6,
+      name: "SPA"
+    }, {
+      id: 7,
+      name: "RUS"
+    }, {
+      id: 8,
+      name: "JAP"
+    }, {
+      id: 9,
+      name: "KOR"
+    }],
+    index: 0,
+    lang: "",
+  },
+  bindPickerChange: function(a) {
+    var e = this;
+    let types = a.currentTarget.id;
+    e.setData({
+      lang: e.data.objectArray[a.detail.value].id
+    }), 
+    wx.chooseImage({
+      count: 1,
+      sizeType: [types],
+      sourceType: ["album", "camera"],
+      success: function(a) {
+        var t = a.tempFilePaths;
+        getApp().globalData.img = t, wx.showLoading({
+          title: "正在识别"
+        }), wx.uploadFile({
+          url: "https://wx.wicode.cn/Wenzi.ashx?lang=" + e.data.lang,
+          filePath: t[0],
+          name: "file",
+          formData: {
+            user: "test"
+          },
+          success: function(a) {
+            wx.hideLoading(), wx.navigateTo({
+              url: "../font/font?list=" + a.data
+            }), console.log(a.data);
+          }
+        });
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
